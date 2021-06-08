19a0cfdde0956d4ae84f456a0d9596c33c12a1d9
aditPrayogo <adit.ihzar@gmail.com>
2021-06-08 21:15:29 +0700

    typographhy done

--- app/src/main/java/com/aditprayogo/gamezin/main/MainActivity.kt
+++ app/src/main/java/com/aditprayogo/gamezin/main/MainActivity.kt
@@ -2,13 +2,50 @@ package com.aditprayogo.gamezin.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.PopupMenu
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.aditprayogo.gamezin.R
+import com.aditprayogo.gamezin.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupNavigationUI()
     }
+
+    private fun setupNavigationUI() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_menu)
+        val menu = popupMenu.menu
+
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.gameFragment,
+                R.id.searchFragment,
+                R.id.favoriteFragment
+            )
+        )
+        binding.bottomNav.setupWithNavController(menu, navController)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.appBarLayout.elevation = 0f
+
+    }
+
+
 }
